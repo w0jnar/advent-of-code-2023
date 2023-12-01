98feb47777bdbf4759fcaf311ffb96705373d3c3
Thomas Wojnar <wojnarthomas@gmail.com>
2023-12-01 02:16:03 -0500

    feat: day01

---
+++ day01/day01.py
@@ -0,0 +1,54 @@
+import pathlib
+
+def total_value_from_file(file_name, parse_digits_out_of_strings = False):
+    total = 0
+    with pathlib.Path(file_name).absolute().open() as f:
+        for line in f:
+            if parse_digits_out_of_strings:
+                line = parse_numbers_in_line(line)
+            temp_string = ''.join(filter(str.isdigit, line))
+            if len(temp_string) == 1:
+                total = total + int(temp_string + temp_string)
+            else:
+                total = total + int(temp_string[0] + temp_string[-1])
+    return total
+
+def parse_numbers_in_line(line):
+    temp_number_list = []
+    i = 0
+    while i < len(line):
+        if line[i].isdigit():
+            temp_number_list.append(line[i])
+        elif line.find('one', i, (i + 3)) >= 0:
+            temp_number_list.append('1')
+        elif line.find('two', i, (i + 3)) >= 0:
+            temp_number_list.append('2')
+        elif line.find('three', i, (i + 5)) >= 0:
+            temp_number_list.append('3')
+        elif line.find('four', i, (i + 4)) >= 0:
+            temp_number_list.append('4')
+        elif line.find('five', i, (i + 4)) >= 0:
+            temp_number_list.append('5')
+        elif line.find('six', i, (i + 3)) >= 0:
+            temp_number_list.append('6')
+        elif line.find('seven', i, (i + 5)) >= 0:
+            temp_number_list.append('7')
+        elif line.find('eight', i, (i + 5)) >= 0:
+            temp_number_list.append('8')
+        elif line.find('nine', i, (i + 4)) >= 0:
+            temp_number_list.append('9')
+        i = i + 1
+    return (''.join(temp_number_list))
+
+if __name__ == "__main__":
+    total = total_value_from_file('day01\\input_example.txt')
+    print(total)
+
+    total = total_value_from_file('day01\\input.txt')
+    print(total)
+
+    total = total_value_from_file('day01\\input_example_two.txt', True)
+    print(total)
+
+    total = total_value_from_file('day01\\input.txt', True)
+    print(total)
