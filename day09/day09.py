879059591392c33a23a2a1ff8c8c427b94406de1
Thomas Wojnar <wojnarthomas@gmail.com>
2023-12-09 23:27:51 -0500

    refactor(day09): simplify

--- day09/day09.py
+++ day09/day09.py
@@ -6,15 +6,14 @@ def extrapolate_lines(sensor_file_name, is_reverse=False):
     output_list = []
     with pathlib.Path(sensor_file_name).absolute().open() as f:
         for line in f:
-            if not is_reverse:
-                output_list.append(calc_next(line))
-            else:
-                output_list.append(calc_previous(line))
+            output_list.append(calc_next(line, is_reverse))
     return reduce((lambda x, y: x + y), output_list)
 
 
-def calc_next(line):
+def calc_next(line, is_reverse):
     line_list = [int(x) for x in line.split()]
+    if is_reverse:
+        line_list.reverse()
     list_of_lists = [line_list]
     i = 0
     # Build the successive lists until we get a list of all 0s.
@@ -35,30 +34,6 @@ def calc_next(line):
             list_of_lists[i][-1] + list_of_lists[i+1][-1])
         i -= 1
     return list_of_lists[0][-1]
-
-
-def calc_previous(line):
-    line_list = [int(x) for x in line.split()]
-    list_of_lists = [line_list]
-    i = 0
-    # Build the successive lists until we get a list of all 0s.
-    while list_of_lists[i].count(0) != len(list_of_lists[i]):
-        next_list = []
-        j = len(list_of_lists[i]) - 1
-        while j >= 1:
-            next_list.insert(0, (list_of_lists[i][j] - list_of_lists[i][j-1]))
-            j -= 1
-        list_of_lists.append(next_list)
-        i += 1
-    # Append a 0 to the list of zeros for calculations.
-    list_of_lists[i].append(0)
-    # Extrapolate, climbing back up the list of lists, skipping the 0 list.
-    i = len(list_of_lists) - 2
-    while i >= 0:
-        list_of_lists[i].insert(
-            0, list_of_lists[i][0] - list_of_lists[i+1][0])
-        i -= 1
-    return list_of_lists[0][0]
 
 
 if __name__ == "__main__":
