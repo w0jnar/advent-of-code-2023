75a5fba548e87592d24fd68615d6e61e6698761c
Thomas Wojnar <wojnarthomas@gmail.com>
2023-12-11 01:54:51 -0500

    refactor(day10): denote private methods

--- day10/day10.py
+++ day10/day10.py
@@ -23,55 +23,55 @@ class PipeMap:
 
     def find_furthest(self):
         # Gets the two positions off of S and traverses the pipes until both routes hit the same pipe, as that would be the furthest pipe.
-        self.find_s()
-        s_pos = self.format_dict_string(self.s_y, self.s_x)
-        current_1, current_2 = self.find_next_from_s()
-        self.find_s_char(current_1, current_2)
+        self._find_s()
+        s_pos = self._format_dict_string(self.s_y, self.s_x)
+        current_1, current_2 = self._find_next_from_s()
+        self._find_s_char(current_1, current_2)
         previous_1 = s_pos
         previous_2 = s_pos
         step = 1
         while current_1 != current_2:
             step += 1
             current_1_holder = current_1
-            current_1 = self.find_next(current_1, previous_1, step)
+            current_1 = self._find_next(current_1, previous_1, step)
             previous_1 = current_1_holder
             current_2_holder = current_2
-            current_2 = self.find_next(current_2, previous_2, step)
+            current_2 = self._find_next(current_2, previous_2, step)
             previous_2 = current_2_holder
         return step
 
-    def find_s(self):
+    def _find_s(self):
         for i, line in enumerate(self.map_list):
             if line.find('S') >= 0:
                 self.s_x = line.find('S')
                 self.s_y = i
                 break
-        self.map_dict[self.format_dict_string(self.s_y, self.s_x)] = 0
+        self.map_dict[self._format_dict_string(self.s_y, self.s_x)] = 0
 
-    def find_next_from_s(self):
+    def _find_next_from_s(self):
         # Find the 2 positions off of S.
         one_positions = []
         if self.s_x - 1 >= 0 and self.map_list[self.s_y][self.s_x - 1] in ('-', 'L', 'F'):
-            current_formatted_string = self.format_dict_string(
+            current_formatted_string = self._format_dict_string(
                 self.s_y, (self.s_x - 1))
             one_positions.append(current_formatted_string)
         if self.s_x + 1 < len(self.map_list[0]) and self.map_list[self.s_y][self.s_x + 1] in ('-', 'J', '7'):
-            current_formatted_string = self.format_dict_string(
+            current_formatted_string = self._format_dict_string(
                 self.s_y, (self.s_x + 1))
             one_positions.append(current_formatted_string)
         if self.s_y - 1 >= 0 and self.map_list[self.s_y - 1][self.s_x] in ('|', '7', 'F'):
-            current_formatted_string = self.format_dict_string(
+            current_formatted_string = self._format_dict_string(
                 (self.s_y - 1), self.s_x)
             one_positions.append(current_formatted_string)
         if self.s_y + 1 < len(self.map_list) and self.map_list[self.s_y + 1][self.s_x] in ('|', 'L', 'J'):
-            current_formatted_string = self.format_dict_string(
+            current_formatted_string = self._format_dict_string(
                 (self.s_y + 1), self.s_x)
             one_positions.append(current_formatted_string)
         for position in one_positions:
             self.map_dict[position] = 1
         return one_positions[0], one_positions[1]
 
-    def find_next(self, current, previous, step):
+    def _find_next(self, current, previous, step):
         # Find the next position, keeping track of the previous to continue progressing.
         current_list = [int(i) for i in current.split('_')]
         previous_list = [int(i) for i in previous.split('_')]
@@ -82,26 +82,30 @@ class PipeMap:
         if (current_x - 1 != previous_x and current_x - 1 >= 0 and
                 self.map_list[current_y][current_x] in ('-', 'J', '7') and
                 self.map_list[current_y][current_x - 1] in ('-', 'L', 'F')):
-            next_position = self.format_dict_string(current_y, (current_x - 1))
+            next_position = self._format_dict_string(
+                current_y, (current_x - 1))
         elif (current_x + 1 != previous_x and current_x + 1 < len(self.map_list[0]) and
                 self.map_list[current_y][current_x] in ('-', 'L', 'F') and
                 self.map_list[current_y][current_x + 1] in ('-', 'J', '7')):
-            next_position = self.format_dict_string(current_y, (current_x + 1))
+            next_position = self._format_dict_string(
+                current_y, (current_x + 1))
         elif (current_y - 1 != previous_y and current_y - 1 >= 0 and
                 self.map_list[current_y][current_x] in ('|', 'L', 'J') and
                 self.map_list[current_y - 1][current_x] in ('|', '7', 'F')):
-            next_position = self.format_dict_string((current_y - 1), current_x)
+            next_position = self._format_dict_string(
+                (current_y - 1), current_x)
         elif (current_y + 1 != previous_y and current_y + 1 < len(self.map_list) and
                 self.map_list[current_y][current_x] in ('|', '7', 'F') and
                 self.map_list[current_y + 1][current_x] in ('|', 'L', 'J')):
-            next_position = self.format_dict_string((current_y + 1), current_x)
+            next_position = self._format_dict_string(
+                (current_y + 1), current_x)
         self.map_dict[next_position] = step
         return next_position
 
-    def format_dict_string(self, y, x):
+    def _format_dict_string(self, y, x):
         return f'{y}_{x}'
 
-    def find(self, position):
+    def _find(self, position):
         # Find in self.map_list with self.map_dict string format.
         position_list = [int(i) for i in position.split('_')]
         return self.map_list[position_list[0]][position_list[1]]
@@ -116,21 +120,21 @@ class PipeMap:
         for i, line in enumerate(map_copy):
             inside = False
             for j, _ in enumerate(line):
-                if self.map_dict.get(self.format_dict_string(i, j), None) is not None:
+                if self.map_dict.get(self._format_dict_string(i, j), None) is not None:
                     if map_copy[i][j] in ('|', 'J', 'L'):
                         inside = not inside
                 else:
                     area += inside
         return area
 
-    def find_s_char(self, current_1, current_2):
+    def _find_s_char(self, current_1, current_2):
         # Figure out which character should be in S position for area.
         # Find the 4 coordinates around S, then figure out where the 2 coords are in regard to S,
         # aka, North/South/East/West of S.
-        north_coord = self.format_dict_string((self.s_y - 1), self.s_x)
-        south_coord = self.format_dict_string((self.s_y + 1), self.s_x)
-        west_coord = self.format_dict_string(self.s_y, (self.s_x - 1))
-        east_coord = self.format_dict_string(self.s_y, (self.s_x + 1))
+        north_coord = self._format_dict_string((self.s_y - 1), self.s_x)
+        south_coord = self._format_dict_string((self.s_y + 1), self.s_x)
+        west_coord = self._format_dict_string(self.s_y, (self.s_x - 1))
+        east_coord = self._format_dict_string(self.s_y, (self.s_x + 1))
         is_north = False
         is_south = False
         is_west = False
